a9e554148266216b7bd0c47bef5f562726500eb3
Metanolia <podogovvv@gmail.com>
2026-04-25 22:29:31 +0300

    Ещё раз убрал лишние комменты

--- hosts/my-laptop.nix
+++ hosts/my-laptop.nix
@@ -1,28 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-my-laptop.nix
-    ];
+  imports = [
+    ./hardware-my-laptop.nix
+  ];
+
+# BOOT
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# NETWORK
 
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+    firewall.enable = true;
+    firewall.checkReversePath = "loose";
+  };
+
+
+# TIME / LOCALE
 
   time.timeZone = "Europe/Moscow";
 
-  i18n.defaultLocale = "ru_RU.UTF-8";
+  i18n = {
+    defaultLocale = "ru_RU.UTF-8";
 
-  i18n.extraLocaleSettings = {
+    extraLocaleSettings = {
     LC_ADDRESS = "ru_RU.UTF-8";
     LC_IDENTIFICATION = "ru_RU.UTF-8";
     LC_MEASUREMENT = "ru_RU.UTF-8";
@@ -33,178 +39,150 @@
     LC_TELEPHONE = "ru_RU.UTF-8";
     LC_TIME = "ru_RU.UTF-8";
   };
+};
+ 
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
+# DESKTOP (KDE PLASMA 6)
+  
+  services.xserver.enable = false;
+  
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-#  services.desktopManager.gnome.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+  services.printing.enable = true;
+ 
+ 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+# SOUND
+
   security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+# BLUETOOTH
+  
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+
+# SECURITY
+
+  security.apparmor.enable = true;
+  
+  services.tailscale.enable = true;
+
+
+# VIRTUALISATION / ISOLATION
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+
+# USERS
+
   users.users.meta = {
     isNormalUser = true;
     description = "Meta";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-
-  environment.systemPackages = with pkgs; [
- wget
- git
- htop
- fish
- distrobox
- telegram-desktop
- vlc
- google-chrome
- python3
- python3Packages.pip
- vscode
- unzip
- curl
- ripgrep
- fd
- bat
- pyenv
- direnv
- qbittorrent
- blueman
- libreoffice-fresh
- btop
- syncthing
- zoxide
- starship
- fastfetch
- eza
- ripgrep
- gitui
- helix
- foliate
- thunderbird
- appimage-run
- github-desktop
- direnv
- nix-direnv
- chromium
- rustdesk-flutter
- exfatprogs
- exfat
-];
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
  
+  shell = pkgs.fish;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  extraGroups = [
+    "wheel"
+    "networkmanager"
+    "podman"
+ ];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11";
+ packages = with pkgs; [
+   kdePackages.kate
+ ];
+};
 
 programs.fish.enable = true;
-users.users.Meta = {
-  isNormalUser = true;
-  shell = pkgs.fish;
-  extraGroups = [ "wheel" "networkmanager" ];
+
+
+# PACKAGES (SYSTEM-WIDE)
+
+environment.systemPackages = with pkgs; [
+  # core utils
+  git wget curl unzip htop btop fastfetch
+
+  # terminal tooling
+  fish starship zoxide eza bat ripgrep fd gitui helix
+
+  # dev 
+  vscode python3 python3Packages.pip pyenv direnv nix-direnv
+
+  # apps
+  telegram-desktop vlc qbittorrent thunderbird foliate libreoffice-fresh
+
+  # browser 
+  google-chrome
+
+  # desktop tools
+  blueman appimage-run github-desktop
+
+  # containers / remote
+  distrobox rustdesk-flutter syncthing
+
+  # filesystems
+  exfatprogs
+
+];
+
+
+# PROGRAMS
+
+programs = {
+   firefox.enable = true;
+   steam.enable = true;
+
+   gamemode.enable = true;
+
+   direnv = {
+     enable = true;
+     nix-direnv.enable = true;
+   };
 };
 
-nix.gc = {
-automatic = true;
-dates = "weekly";
-options = "--delete-older-than 7d";
+
+
+# NIX SETTINGS
+
+nix = {
+  settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+  };
+
+  gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 3d";
+  };
+
 };
 
-programs.fish.interactiveShellInit = '' '';
+nixpkgs.config.allowUnfree = true;
 
-programs.direnv.enable = true;
-programs.direnv.nix-direnv.enable = true;
-programs.steam = { enable = true;
-remotePlay.openFirewall = true;
-dedicatedServer.openFirewall = true;
-};
+# SYSTEM VERSION
 
-programs.gamemode.enable = true;
+system.stateVersion = "25.11";
 
-virtualisation.podman = { enable = true;
-dockerCompat = true; };
-
-# services.v2raya.enable = true;
-
-hardware.bluetooth.enable = true;
-hardware.bluetooth.powerOnBoot = true;
-
-services.tailscale.enable = true;
-
-programs.throne.tunMode.enable = true;
-
-
-services.udev.packages = [ pkgs.direnv ];
-programs.bash.interactiveShellInit = ''
-  eval "$(direnv hook bash)"
-'';
-
-nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
 }
